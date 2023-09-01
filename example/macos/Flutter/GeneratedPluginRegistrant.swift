2aa5c398e227984135942aa4f80dec32706c5986
masjid al hijrah <masjidalhijrah@MacBook-Pro-masjid.local>
2023-09-02 02:13:56 +0700

    Update

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import adhan_prayer
-import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AdhanPrayerPlugin.register(with: registry.registrar(forPlugin: "AdhanPrayerPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <adhan_prayer/adhan_prayer_plugin_c_api.h>
-#include <geolocator_windows/geolocator_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AdhanPrayerPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AdhanPrayerPluginCApi"));
-  GeolocatorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("GeolocatorWindows"));
 }
